695095d18bd28c0df4cbb9496cb54decceb149ba
Jeong-ki <7863z@naver.com>
2022-01-10 00:52:52 +0900

    ✨ 이미지 슬라이드 구현

--- js/postUpload.js
+++ js/postUpload.js
@@ -7,18 +7,24 @@ function readImage(input) {
         container.removeChild(container.firstChild);
     }
 
+    const fileArr = Array.from(input.files);
+
+    if(fileArr.length > 3) {
+        return alert("최대 3개까지 업로드 가능합니다");
+    }
+    
     if(input.files.length) {
         console.log(input.files);
 
-        const fileArr = Array.from(input.files);
-        const row = document.createElement('li');
+        const row = document.createElement('div');
         row.classList.add('row');
 
         fileArr.forEach((file, index) => {
             const reader = new FileReader();
             const imgDiv = document.createElement('div');
-            const btnX = document.createElement('button');
+            const btnX = document.createElement('div');
             const img = document.createElement('img');
+            btnX.classList.add('btnX');
             img.classList.add('image');
 
             const label = document.createElement('label');
@@ -31,26 +37,97 @@ function readImage(input) {
             reader.onload = e => {
                 img.src = e.target.result;
                 if(input.files.length > 1) {
-                    imgDiv.style.width = "168px";
-                    imgDiv.style.height = "126px";
+                    imgDiv.firstChild.style.width = "168px";
+                    imgDiv.firstChild.style.height = "126px";
                 } else {
-                    imgDiv.style.width = "304px";
-                    imgDiv.style.height = "228px";
+                    imgDiv.firstChild.style.width = "304px";
+                    imgDiv.firstChild.style.height = "228px";
                 }
             }
-
-            console.log(file.name);
             row.appendChild(imgDiv);
             reader.readAsDataURL(file);
         });
         container.appendChild(row);
     }
+
+    // remove upload image
+    const btnDel = document.querySelectorAll('.btnX');
+    let btnDelArr = Array.prototype.slice.call(btnDel);
+    btnDel.forEach((element) => {
+        element.addEventListener('click', e => {
+            const delIndex = btnDelArr.indexOf(element);
+            element.parentNode.remove();
+            fileArr.splice(delIndex, 1);
+            btnDelArr.splice(delIndex, 1);
+        })
+    });
+
+    if(fileArr.length === 1) {
+        container.style.height = "228px";
+        container.firstChild.style.width = "304px";
+        container.firstChild.style.height = "228px";
+    } else if(fileArr.length === 2) {
+        container.style.height = "126px";
+        container.firstChild.style.width = "344px";
+        container.firstChild.style.height = "126px";
+    } else if(fileArr.length === 3) {
+        container.style.height = "126px";
+        container.firstChild.style.width = "520px";
+        container.firstChild.style.height = "126px";
+    }
+
+    imgSlider();
 }
 
-const inputImage = document.querySelector('#img-upload')
+const inputImage = document.querySelector('#img-upload');
 inputImage.addEventListener('change', e => {
     readImage(e.target);
-})
+});
 
 
-// 2. 업로드할 이미지 제거
+// 2. 업로드 이미지 슬라이더
+function imgSlider() {
+    let slider = document.querySelector(".img-container");
+    let innerSlider = document.querySelector(".row");
+    let pressed = false;
+    let startx;
+    let x;
+    
+    slider.addEventListener("mousedown", e => {
+      pressed = true;
+      startx = e.offsetX - innerSlider.offsetLeft;
+      slider.style.cursor = "grabbing";
+    })
+    
+    slider.addEventListener("mouseenter", () => {
+      slider.style.cursor = "grab";
+    })
+    
+    slider.addEventListener("mouseup", () => {
+      slider.style.cursor = "grab";
+    })
+    
+    window.addEventListener("mouseup", () => {
+      pressed = false;
+    })
+    
+    slider.addEventListener("mousemove", e => {
+      if (!pressed) return;
+      e.preventDefault();
+      x = e.offsetX;
+    
+      innerSlider.style.left = `${x - startx}px`;
+      checkboundary();
+    })
+    
+    function checkboundary() {
+      let outer = slider.getBoundingClientRect();
+      let inner = innerSlider.getBoundingClientRect();
+    
+      if (parseInt(innerSlider.style.left) > 0) {
+        innerSlider.style.left = "0px";
+      } else if (inner.right < outer.right) {
+        innerSlider.style.left = `-${inner.width - outer.width}px`;
+      }
+    }
+}
