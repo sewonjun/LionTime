05ac114b97f0cd587c3d444ec177f20975aa0c97
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-01-17 04:49:03 +0900

    💄  가운데 정렬  + reset.css 추가

--- js/loginEmail.js
+++ js/loginEmail.js
@@ -3,40 +3,39 @@
 //     console.log(document.querySelector("#loginPWInput").value)
 // }
 
-const email = document.querySelector('#idEmailInput')
-const pw = document.querySelector('#loginPWInput')
+const email = document.querySelector('#idEmailInput');
+const pw = document.querySelector('#loginPWInput');
 
 // 비동기로 동하는것을 명시해줘야 await을 할 수 있다!
 async function login() {
-    const email = document.querySelector("#idEmailInput").value
-    const pw = document.querySelector("#loginPWInput").value
-    const url = "http://146.56.183.55:5050"
+    const email = document.querySelector('#idEmailInput').value;
+    const pw = document.querySelector('#loginPWInput').value;
+    const url = 'http://146.56.183.55:5050';
     const loginData = {
-        "user": {
-            "email": email,
-            "password": pw
-        }
-    }
+        user: {
+            email: email,
+            password: pw,
+        },
+    };
 
     const res = await fetch(url + '/user/login', {
         //메소드 구분
-        method: "POST",
+        method: 'POST',
         //헤더
         headers: {
-            "Content-type": "application/json"
+            'Content-type': 'application/json',
         },
         //이건 오브잭트를 문자열로 바꿔주는 부분
-        body: JSON.stringify(loginData)
-    })
-    const json = await res.json();//외않됌? 포인트 res.json()도 비동기. await을 해줘야한다.
+        body: JSON.stringify(loginData),
+    });
+    const json = await res.json(); //외않됌? 포인트 res.json()도 비동기. await을 해줘야한다.
     // console.log(json);
     // console.log(json.user.token);
     // console.log(json.user.accountname);
-    sessionStorage.setItem("Token", json.user.token)
-    sessionStorage.setItem("_id", json.user._id)
-    sessionStorage.setItem("accountname", json.user.accountname)
-
-    location.href = "home.html"
+    sessionStorage.setItem('Token', json.user.token);
+    sessionStorage.setItem('_id', json.user._id);
+    sessionStorage.setItem('accountname', json.user.accountname);
+    location.href = 'home.html';
 }
-const $loginBtn = document.querySelector('#submitBtn')
-$loginBtn.addEventListener("click", login)
+const $loginBtn = document.querySelector('#submitBtn');
+$loginBtn.addEventListener('click', login);
