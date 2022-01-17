6e65c42deedf059a0568c2b27da13a5447e215fa
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-01-17 22:00:23 +0900

    📝 login 유효성 검사 완료

--- js/loginEmail.js
+++ js/loginEmail.js
@@ -5,6 +5,32 @@
 
 const email = document.querySelector('#idEmailInput');
 const pw = document.querySelector('#loginPWInput');
+const loginForm = document.querySelector('.loginForm');
+const $loginBtn = document.querySelector('#submitBtn');
+const errorMessage = document.querySelector('.loginError');
+email.addEventListener('input', () => {
+    if (email.value !== '') {
+        if (pw.value !== '') {
+            $loginBtn.disabled = false;
+        } else {
+            $loginBtn.disabled = true;
+        }
+    } else {
+        $loginBtn.disabled = true;
+    }
+});
+
+pw.addEventListener('input', () => {
+    if (pw.value !== '') {
+        if (email.value !== '') {
+            $loginBtn.disabled = false;
+        } else {
+            $loginBtn.disabled = true;
+        }
+    } else {
+        $loginBtn.disabled = true;
+    }
+});
 
 // 비동기로 동하는것을 명시해줘야 await을 할 수 있다!
 async function login() {
@@ -29,13 +55,19 @@ async function login() {
         body: JSON.stringify(loginData),
     });
     const json = await res.json(); //외않됌? 포인트 res.json()도 비동기. await을 해줘야한다.
-    // console.log(json);
+    console.log(json);
     // console.log(json.user.token);
     // console.log(json.user.accountname);
-    sessionStorage.setItem('Token', json.user.token);
-    sessionStorage.setItem('_id', json.user._id);
-    sessionStorage.setItem('accountname', json.user.accountname);
-    location.href = 'home.html';
+    if (json.message === '이메일 또는 비밀번호가 일치하지 않습니다.') {
+        errorMessage.innerText = '*이메일 또는 비밀번호가 일치하지 않습니다.';
+    } else {
+        sessionStorage.setItem('Token', json.user.token);
+        sessionStorage.setItem('_id', json.user._id);
+        sessionStorage.setItem('accountname', json.user.accountname);
+        location.href = 'home.html';
+    }
 }
-const $loginBtn = document.querySelector('#submitBtn');
+
+// blur, disable 풀기
+
 $loginBtn.addEventListener('click', login);
